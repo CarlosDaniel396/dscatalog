605b4d62ea17169281f6a9a0140c8c65c6f1b13a
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-06-27 09:43:47 -0300

    feat: find-category-by-id

--- backend/src/main/java/com/carlos/catalog/services/CategoryService.java
+++ backend/src/main/java/com/carlos/catalog/services/CategoryService.java
@@ -1,5 +1,7 @@
 package com.carlos.catalog.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.carlos.catalog.dto.CategoryDTO;
 import com.carlos.catalog.entities.Category;
 import com.carlos.catalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -20,5 +23,12 @@ public class CategoryService {
 	public Page<CategoryDTO> findAllPaged(Pageable pageable) {
 		Page<Category> list = repository.findAll(pageable);
 		return list.map(x -> new CategoryDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 }
