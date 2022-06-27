b6064d6bc113edf30bca1e26ac16fcdc565b3919
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-06-27 11:39:18 -0300

    validation

---
+++ backend/src/main/java/com/carlos/catalog/dto/UserUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.carlos.catalog.dto;
+
+import com.carlos.catalog.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+
+	
+}
