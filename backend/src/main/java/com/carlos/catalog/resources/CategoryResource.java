9651845599152ce252ab5c6ecfdd68a2b03272a4
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-06-27 09:41:40 -0300

    feat: find-all-categories-paged

---
+++ backend/src/main/java/com/carlos/catalog/resources/CategoryResource.java
@@ -0,0 +1,27 @@
+package com.carlos.catalog.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlos.catalog.dto.CategoryDTO;
+import com.carlos.catalog.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@Autowired
+	private CategoryService service;
+
+	@GetMapping
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list);
+	}
+
+}
