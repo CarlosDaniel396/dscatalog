f79668bf1c6bd9fc76c4606fd74ba52f0f8b65dd
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-07-07 08:53:00 -0300

    Catalog loader

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -7,12 +7,14 @@ import ProductCard from 'components/ProductCard';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BASE_URL } from 'util/requests';
+import CardLoader from './CardLoader';
 
 import './styles.css';
 
 const Catalog = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
-
+  const [isLoading, setIsLoading] = useState(false);
+ 
   useEffect(() => {
     const params: AxiosParams = {
       method: 'GET',
@@ -23,8 +25,13 @@ const Catalog = () => {
       },
     };
 
-    axios(params).then((response) => {
-      setPage(response.data);
+    setIsLoading(true);
+    axios(params)
+      .then((response) => {
+        setPage(response.data);
+    })
+    .finally(() => {
+      setIsLoading(false);
     });
   }, []);
 
@@ -35,13 +42,14 @@ const Catalog = () => {
       </div>
 
       <div className="row">
-        {page?.content.map((product) => (
+        {isLoading ? <CardLoader /> : (
+          page?.content.map((product) => (
           <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
             <Link to="/products/1">
               <ProductCard product={product} />
             </Link>
           </div>
-        ))}
+        )))}
       </div>
 
       <div className="row">
